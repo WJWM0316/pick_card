038ed5cfdb1313ae227757989950b27b6edd693e
DESKTOP-LRMLL2E\42902 <wwx@theger.com.cn>
2018-07-23 21:58:31 +0800

    1

--- src/api/require.js
+++ src/api/require.js
@@ -29,7 +29,7 @@ export const request = ({ method = 'post', url, data = {}, needKey = true, isLoa
     util.loading(isLoading)
     const addHttpHead = {
       token: wx.getStorageSync('token') || '',
-      cv: 103
+      cv: 104
     }
     wx.request({
       url: config.baseHost+url,
